fd26e7750b0e4a11feaf35a737dc85b79518e04e
rebs-code <jack.rebussi@gmail.com>
2023-11-07 18:04:10 +0100

    feat: complete script

--- js/main.js
+++ js/main.js
@@ -21,17 +21,34 @@ generateButton.addEventListener("click", function () {
         tier = "Biglietto Standard";
         document.getElementById("tier").innerText = tier; 
     };
-
-    let carriageNum = String(Math.floor(Math.random() * 100000)).padStart(5, '0');
+    //generate a random carriage number
+    let carriageNum = Math.floor(Math.random() * 10 + 1);
     document.getElementById("carriage").innerText = carriageNum;
-
+    //generate a random number for the code
+    let codeNum = String(Math.floor(Math.random() * 100000)).padStart(5, '0');
+    document.getElementById("code").innerText = codeNum;
+    //insert passenger name on ticket
     document.getElementById("passenger-name").innerText = name;
-
-
-
-    console.log("Nome:", name);
-    console.log("Chilometri da Percorrere:", distance);
-    console.log("Fascia età:", age);
+    //calculate ticket price
+    const priceKm = 0.21;
+    const totalTicketPrice = distance * priceKm;
+    //apply discount if needed
+    let discount = 0;
+    if (age =="minorenne") {
+        discount = totalTicketPrice * 0.20;
+    } else if (age == "over65") {
+        discount = totalTicketPrice * 0.40;
+    };
+    //calculate final price with discount
+    const finalTicketPrice = totalTicketPrice - discount;
+    //insert price on ticket
+    document.getElementById("price").innerText = `€${finalTicketPrice.toFixed(2)}`;
+    //show ticket title
+    const ticketTitle = document.getElementById("ticket-title");
+    ticketTitle.classList.remove("hidden");
+    //show ticket
+    const ticket = document.getElementById("ticket-info");
+    ticket.classList.remove("hidden");
 });
 
 // cancel button
@@ -47,5 +64,11 @@ cancelButton.addEventListener("click", function () {
     document.getElementById("carriage").innerText = "";
     document.getElementById("code").innerText = "";
     document.getElementById("price").innerText = "";
+    //hide ticket title
+    const ticketTitle = document.getElementById("ticket-title");
+    ticketTitle.classList.add("hidden");
+    //hide ticket
+    const ticket = document.getElementById("ticket-info");
+    ticket.classList.add("hidden");
 });
 
