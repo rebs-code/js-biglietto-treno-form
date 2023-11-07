d9ed810e73d0d9002efce25abfb861af7b4f3159
rebs-code <jack.rebussi@gmail.com>
2023-11-07 17:43:18 +0100

    feat: wire ticket offer & carriage n generator

--- js/main.js
+++ js/main.js
@@ -5,14 +5,33 @@
 const generateButton = document.getElementById("generate-button");
 
 generateButton.addEventListener("click", function () {
+
     const name = document.getElementById("name").value;
     const distance = document.getElementById("distance").value;
     const age = document.getElementById("age").value;
 
+    let tier;
+    if (age == "minorenne") {
+        tier = "Offerta Minori";
+        document.getElementById("tier").innerText = tier; 
+    } else if (age == "over65") {
+        tier = "Offerta Over 65";
+        document.getElementById("tier").innerText = tier; 
+    } else {
+        tier = "Biglietto Standard";
+        document.getElementById("tier").innerText = tier; 
+    };
+
+    let carriageNum = String(Math.floor(Math.random() * 100000)).padStart(5, '0');
+    document.getElementById("carriage").innerText = carriageNum;
+
+    document.getElementById("passenger-name").innerText = name;
+
+
+
     console.log("Nome:", name);
     console.log("Chilometri da Percorrere:", distance);
     console.log("Fascia età:", age);
-
 });
 
 // cancel button
@@ -23,5 +42,10 @@ cancelButton.addEventListener("click", function () {
     document.getElementById("name").value = "";
     document.getElementById("distance").value = "";
     document.getElementById("age").selectedIndex = 0;
+    document.getElementById("passenger-name").innerText = "";
+    document.getElementById("tier").innerText = "";
+    document.getElementById("carriage").innerText = "";
+    document.getElementById("code").innerText = "";
+    document.getElementById("price").innerText = "";
 });
 
